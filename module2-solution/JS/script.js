950dad3c2a6097709c9ed5b7771f3b6d4b29630c
Andrew <andrewashrafattia15@gmail.com>
2021-09-19 22:37:20 +0200

    module-4 sol

---
+++ module2-solution/JS/script.js
@@ -0,0 +1,41 @@
+// var string = "hello";
+// string += "World !";
+//  $y = 2 ;
+// string = "2" ;
+// if(string == y ){
+//     console.log(string + "=" + y); 
+// }else{
+//     if(string === y){
+       
+//     }
+//     else{
+//         console.log(string + "! =  " + y); 
+//     }
+// }
+    //  var facebook = {
+    //      firstName: "Facebook" ,
+    //      ceo: {
+    //          firstname:"Mark" ,
+    //          $Stock: 110  
+    //      },
+    //      $value: "Ahmaed"
+    //  };
+    //  console.log(facebook);
+
+    var computer = {name:"Andrew" , charactersitics: {ram:"15" , "Name of company" :"andreww"}};
+    // computer.name = "Andrew";
+    // computer.charactersitics = new Object ();
+    // computer.charactersitics.ram = "24 GB";
+    // computer.charactersitics.speed = "2 dk/f";
+    // console.log(computer.charactersitics.ram);
+
+    //    function multiply (x,y){
+    //        return x*y ;
+    //    };
+    console.log(multiply(2,3));
+    //    console.log(computer["charactersitics"]);
+    //    console.log(computer.charactersitics["ram"]);
+    console.log(computer);
+        var x = computer ;
+        x.charactersitics["Name of company"] = "DODZ";
+        console.log(computer); 
